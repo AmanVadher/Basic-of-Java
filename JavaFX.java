61da413bf5e4fd0e59a8e284bbff38da0cde6cee
Aman Vadher <126086528+AmanVadher@users.noreply.github.com>
2025-03-26 14:17:14 +0530

    JavaFX.java

--- JavaFX.java
+++ JavaFX.java
@@ -1,1 +1,8 @@
-import javafx
+public class JavafxSample extends Application {  
+   @Override     
+   public void start(Stage primaryStage) throws Exception {     
+   }         
+   public static void main(String args[]){           
+      launch(args);      
+   } 
+} 
