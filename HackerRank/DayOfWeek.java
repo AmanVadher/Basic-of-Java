3e6cb83fd89cbac97dc07b9911b28f9965693dd9
AmanVadher <amanvadher110@gmail.com>
2025-08-07 16:45:39 +0530

    Modify Features

--- HackerRank/DayOfWeek.java
+++ HackerRank/DayOfWeek.java
@@ -1,16 +1,5 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 import java.time.*;
-import java.time.format.*;
 class Result {
 
     /*
