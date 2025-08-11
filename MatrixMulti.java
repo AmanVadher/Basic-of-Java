6fb4303b463d5d68c876849a2158a6bd00217595
AmanVadher <amanvadher110@gmail.com>
2025-08-11 17:00:16 +0530

    Feature Update

--- MatrixMulti.java
+++ MatrixMulti.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class MatrixMulti {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter elements for first matrix:");
+        System.out.println("Enter elements:");
         int[][] matrix1 = new int[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 matrix1[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Enter elements for second matrix:");
+        System.out.println("Enter elements:");
         int[][] matrix2 = new int[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
